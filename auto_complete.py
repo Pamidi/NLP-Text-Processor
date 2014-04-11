20a70193d07b75972c00ab9390474a46c4f27a0b
xavier <josexavier06@gmail.com>
2014-04-11 23:45:04 +0530

    pushing nlp project

---
+++ auto_complete.py
@@ -0,0 +1,116 @@
+class Node:     
+    def __init__(self,ch,flag): # Constructor for Node Object
+        self.ch = ch
+        self.flag = flag
+        self.left = 0
+        self.right = 0
+        self.center = 0                    
+
+    def Add(self,string,node): # Function to add a string 
+
+        key = string[0] 
+        
+        if node == 0 :
+            node = Node(key,0) 
+
+        if key < node.ch :          
+            node.left = node.Add(string,node.left)  
+
+        elif key > node.ch :          
+            node.right = node.Add(string,node.right)
+
+        else :  
+            if len(string) == 1 :
+                node.flag = 1  
+            else : node.center = node.Add(string[1:],node.center)
+            
+        return node    
+
+    def spdfs(self,match):  # DFS for Ternary Search Tree
+        
+        if self.flag == 1 : 
+            print("Match : ",match)  
+            
+        if self.center == 0 and self.left == 0 and self.right == 0:            
+            return  
+                         
+        if self.center != 0 :            
+            self.center.spdfs(match + self.center.ch)
+            
+            
+        if self.right != 0 :         
+            self.right.spdfs(match[:-1]+self.right.ch)            
+            
+        if self.left != 0 :            
+            self.left.spdfs(match[:-1]+self.left.ch)  
+
+    def simple(self,string):  # Function to search a string in the Ternary Search Tree
+        temp = self
+        i=0
+        while temp != 0 :
+            if (string[i] < temp.ch) :  temp = temp.left;
+            elif(string[i] > temp.ch) : temp = temp.right;
+            else :
+                i=i+1              
+                if(i == len(string)):
+                    return temp.flag 
+                temp = temp.center
+
+        return 0
+            
+    
+        
+    def search(self,string,match):
+        # Function to implement Auto complete search
+    
+        if len(string) > 0:
+            key = string[0]
+
+            if key < self.ch :
+                if(self.left == 0):
+                    print("No Match Found")
+                    return                            
+                self.left.search(string,match)
+
+            elif key > self.ch :
+                if(self.right == 0):
+                    print("Not Match Found")
+                    return
+                self.right.search(string,match)
+
+            else :                
+                if len(string) == 1:                                         
+                    if self.flag == 1 : print("Match ",match+self.ch)
+                    if self.center != 0 :
+                        self.center.spdfs(match+self.ch+self.center.ch)
+                    return 1                
+                self.center.search(string[1:],match+key)                        
+            
+        else :
+            print("Invalid String")
+            return
+            
+def fileparse(filename,node):
+
+    #Parse the Input Dict file and build the TST   
+    fd = open(filename)    
+    line = fd.readline().strip('\r\n') 
+    while line !='':
+        
+        node.Add(line,node)
+        line = fd.readline().strip('\r\n')
+
+
+if __name__=='__main__':
+    
+    root = Node('',0)
+
+    # Give the Path to the Dictionary File in
+    Path_to_dict = "english_words.txt"
+    
+    fileparse(Path_to_dict,root)
+    
+    inp = ''
+    while inp !='q':
+        inp = raw_input("Enter String : ",)
+        root.search(inp,'') 
---
+++ grammar_validator.py
@@ -0,0 +1,119 @@
+import nltk
+
+sent = nltk.word_tokenize("I like a girl")
+tagged_text = nltk.pos_tag(sent)
+pos_tags = [pos for (token,pos) in nltk.pos_tag(sent)]
+
+var_s="S -> NP VP"
+var_pp="PP -> P NP | P VP"
+var_np="NP -> N | Det N | Det N P"
+var_vp="VP -> V NP | VP PP | V PP | V"
+var_n=""
+var_v=""
+var_p=""
+var_det=""
+var_others=""
+
+k=-1
+for j in pos_tags:
+   k=k+1
+   i=sent[k]
+   print(j)
+   if j=='DT' or j=='PRP$':
+     if(len(var_det)==0):
+        var_det = var_det+'Det -> \''+i+'\''
+     else:
+        var_det = var_det+' | \''+i+'\''
+   elif j=='NNP' or j=='PRP' or j=='NNS':
+        var_np = var_np+' | \''+i+'\''
+   elif j=='JJ' or j=='NN':
+     if(len(var_n)==0):
+        var_n = var_n+'N -> \''+i+'\''
+     else:
+        var_n = var_n+' | \''+i+'\''
+   elif j=='VBZ' or j=='VBP' or j=='VBD' or j=='VB':
+     if(len(var_v)==0):
+        var_v = var_v+'V -> \''+i+'\''
+     else:
+        var_v = var_v+' | \''+i+'\''
+   elif j=='PP' or j=='IN' or j=='TO':
+     if(len(var_p)==0):
+        var_p = var_p+'P -> \''+i+'\''
+     else:
+        var_p = var_p+' | \''+i+'\''
+
+if(len(var_n)==0):
+   var_n='N -> \'aaa\''
+if(len(var_v)==0):
+   var_v='V -> \'aaa\''
+if(len(var_p)==0):
+   var_p='P -> \'aaa\''
+if(len(var_det)==0):
+   var_det='Det -> \'aaa\''
+
+
+with open("my_grammar.cfg", "w") as myfile:
+    myfile.write(var_s)
+    myfile.write('\n')
+    myfile.write(var_pp)
+    myfile.write('\n')
+    myfile.write(var_np)
+    myfile.write('\n')
+    myfile.write(var_vp)
+    myfile.write('\n')
+    myfile.write(var_n) 
+    myfile.write('\n')
+    myfile.write(var_v) 
+    myfile.write('\n')
+    myfile.write(var_det) 
+    myfile.write('\n')
+    myfile.write(var_p) 
+    
+simple_grammar = nltk.data.load('file:my_grammar.cfg')
+#parser = nltk.ChartParser(simple_grammar)
+#tree = parser.nbest_parse(sent)
+#for t in tree:
+#     print t
+
+  
+def init_wfst(tokens, grammar):
+    numtokens = len(tokens)
+    wfst = [[None for i in range(numtokens+1)] for j in range(numtokens+1)]
+    for i in range(numtokens):
+        productions = grammar.productions(rhs=tokens[i])
+        wfst[i][i+1] = productions[0].lhs()
+    return wfst
+
+def complete_wfst(wfst, tokens, grammar, trace=False):
+    index = dict((p.rhs(), p.lhs()) for p in grammar.productions())
+    numtokens = len(tokens)
+    for span in range(2, numtokens+1):
+        for start in range(numtokens+1-span):
+            end = start + span
+            for mid in range(start+1, end):
+                nt1, nt2 = wfst[start][mid], wfst[mid][end]
+                if nt1 and nt2 and (nt1,nt2) in index:
+                    wfst[start][end] = index[(nt1,nt2)]
+                    if trace:
+                        print "[%s] %3s [%s] %3s [%s] ==> [%s] %3s [%s]" % \
+                        (start, nt1, mid, nt2, end, start, index[(nt1,nt2)], end)
+    return wfst
+
+def display(wfst, tokens):
+    print '\nWFST ' + ' '.join([("%-4d" % i) for i in range(1, len(wfst))])
+    for i in range(len(wfst)-1):
+        print "%d   " % i,
+        for j in range(1, len(wfst)):
+            print "%-4s" % (wfst[i][j] or '.'),
+        print
+
+wfst0 = init_wfst(sent, simple_grammar)
+wfst1 = complete_wfst(wfst0, sent, simple_grammar)
+print
+display(wfst1,sent)
+print
+str= '%r' % (wfst1[0][len(sent)])
+if (str=='S'):
+   print("valid sentence")
+else:
+   print("invalid sentence")
